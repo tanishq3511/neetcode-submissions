bceaef6267297307a6232f5e22a6786cbb1b0983
Tanishq Akasapu <tanishq3511@gmail.com>
2026-08-12 15:58:18 -0500

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.java
@@ -0,0 +1,39 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(min.isEmpty() || val <= min.peek()){
+            min.push(val);
+        }
+    }
+    
+    public void pop() {
+        // if(stack.isEmpty()) return;
+        // if(stack.peek() == min.peek()){
+        //     min.pop();
+        // }
+        // stack.pop();
+
+        if (stack.isEmpty()) return;
+        int top = stack.pop();
+        if (top == min.peek()) {
+            min.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
