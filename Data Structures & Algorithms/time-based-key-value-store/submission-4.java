aa5d0869321cc453e82cc64bd8807e6bed741f38
Tanishq Akasapu <tanishq3511@gmail.com>
2026-08-19 17:32:47 -0500

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.java
@@ -0,0 +1,26 @@
+class TimeMap {
+
+    HashMap<String, TreeMap<Integer, String>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)){
+            return "";
+        }
+
+        TreeMap<Integer, String> timestamps = map.get(key);
+        Map.Entry<Integer, String> entry = timestamps.floorEntry(timestamp);
+        if(entry == null){
+            return "";
+        }else{
+            return entry.getValue();
+        }
+    }
+}
